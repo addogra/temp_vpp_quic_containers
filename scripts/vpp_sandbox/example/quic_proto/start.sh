12da9bd8d2e71e2458c8e0ee89095f3281281690
addogra <dogra.aditya@gmail.com>
2018-03-27 05:48:42 -0400

    adding delay in vpp container creation and fixed the path

--- scripts/vpp_sandbox/example/quic_proto/start.sh
+++ scripts/vpp_sandbox/example/quic_proto/start.sh
@@ -4,7 +4,9 @@ sudo ./launch_lxc.sh -n r -t vpp-ext_quic -l 1
 sudo ./launch_lxc.sh -n s -t vpp-ext_quic -l 1
 
 #Connecting to the container 
-sudo lxc-attach -n r -- vpp unix { log /tmp/vpp.log full-coredump startup-config /scratch/example/quic-proto/r.conf cli-listen localhost:5002 }
-sudo lxc-attach -n s -- vpp unix { log /tmp/vpp.log full-coredump startup-config /scratch/example/quic-proto/s.conf cli-listen localhost:5002 }
+sleep 20
+sudo lxc-attach -n r -- vpp unix { log /tmp/vpp.log full-coredump startup-config /scratch/example/quic_proto/r.conf cli-listen localhost:5002 }
+sleep 30
+sudo lxc-attach -n s -- vpp unix { log /tmp/vpp.log full-coredump startup-config /scratch/example/quic_proto/s.conf cli-listen localhost:5002 }
 
 
